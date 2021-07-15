24fc3152d8fd01970ac092f258e435408d250c3d
Igor dos Santos Tessaro <santos.igor@cwi.com.br>
2021-07-14 21:02:58 -0300

    feature(unittests): create unittest project and unittests from person service

---
+++ AdventureWorks.Person.Domain.Tests/Services/PersonServiceTests.cs
@@ -0,0 +1,79 @@
+﻿using AdventureWorks.Person.Domain.Services;
+using AdventureWorks.Person.Domain.Services.Abstraction;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AdventureWorks.Person.Domain.Tests.Services
+{
+    [TestClass]
+    public sealed class PersonServiceTests
+    {
+        private IPersonService _personService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            this._personService = new PersonService();
+        }
+
+        [TestMethod]
+        public void CreateWhenWithoutFirstNameShouldReturnNotificationWithValidation()
+        {
+            // Arrange
+            string firstName = string.Empty;
+            string middleName = "middle-name";
+            string lastName = "last-name";
+
+            // Act
+            var result = this._personService.Create(firstName, middleName, lastName);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CreateWhenWithoutMiddleNameShouldReturnNotificationWithValidation()
+        {
+            // Arrange
+            string firstName = "first-name";
+            string middleName = string.Empty;
+            string lastName = "last-name";
+
+            // Act
+            var result = this._personService.Create(firstName, middleName, lastName);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CreateWhenWithoutLastNameShouldReturnNotificationWithValidation()
+        {
+            // Arrange
+            string firstName = "first-name";
+            string middleName = "middle-name";
+            string lastName = string.Empty;
+
+            // Act
+            var result = this._personService.Create(firstName, middleName, lastName);
+
+            // Assert
+            result.IsValid.Should().BeFalse();
+        }
+
+        [TestMethod]
+        public void CreateWhenAllParametersValidShouldReturnNotificationValid()
+        {
+            // Arrange
+            string firstName = "first-name";
+            string middleName = "middle-name";
+            string lastName = "last-name";
+
+            // Act
+            var result = this._personService.Create(firstName, middleName, lastName);
+
+            // Assert
+            result.IsValid.Should().BeTrue();
+        }
+    }
+}
